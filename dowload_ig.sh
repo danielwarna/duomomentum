c16dd6db9cc9fa3ca16a2d2416ff577d8ee43d41
Daniel Wärnå <daniel@danielwarna.com>
2023-09-13 00:49:14 +0300

    Load instagram content using instaloader Improved feedback view

---
+++ dowload_ig.sh
@@ -0,0 +1,4 @@
+# Install instaloader in venv, pip install instaloader
+# https://instaloader.github.io/cli-options.html
+
+instaloader profile acroduo.momentum --post-filter="not is_video" --count 6 --no-captions --no-compress-json --no-metadata-json --dirname-pattern
